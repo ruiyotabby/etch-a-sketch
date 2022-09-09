adc300f3b9dcba38d42cf6e0fbaf8be5a99aa93e
Ruiyotabby <ruiyotabby@gmail.com>
2022-09-09 17:52:34 +0300

    Create a random color function for the grid and background

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let bod = document.querySelector('body')
 
 let squareGrid = (num) => {
     for(let i = 0; i < num; i++){
-        const colDiv = document.createElement('div');
+        let colDiv = document.createElement('div');
         colDiv.classList.add('row')
         colDiv.setAttribute(`style`, `width: ${0.6*num}em;`)
         container.appendChild(colDiv);
         
         for(let j = 0; j < num; j++){
-            const rowDiv = document.createElement('div');
+            let rowDiv = document.createElement('div');
             rowDiv.classList.add('col');
             rowDiv.setAttribute(`style`, `width: ${num}em;`)
             colDiv.appendChild(rowDiv);
@@ -17,4 +17,24 @@ let squareGrid = (num) => {
     }
 }
 
-squareGrid(64)
+squareGrid(10)
+
+let rowDiv = document.getElementsByClassName('col')
+
+for(let k = 0; k < rowDiv.length; k++){
+    rowDiv[k].addEventListener('mouseover',(e) => {
+        e.target.style.backgroundColor = randomRgb();
+    })
+}
+
+let randomRgb = () => {
+    var r = Math.round, g = Math.random, c = 255;
+    return `rgb(${r(g()*c)},${r(g()*c)},${r(g()*c)})`;
+}
+
+if(window.performance.navigation && window.performance.navigation.type === 1 || 
+    window.performance.getEntriesByType('navigation')
+    .map((nav) => nav.type).includes('reload')){
+    bod.style.background = randomRgb();
+}
+container.style.backgroundColor = 'white';
